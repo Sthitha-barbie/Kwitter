9516f93ca963f533c9552fb278b6959d6a8f85fd
Sthitha-barbie <80837183+Sthitha-barbie@users.noreply.github.com>
2021-09-25 17:42:43 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,10 +1,13 @@
-
-//ADD YOUR FIREBASE LINKS HERE
-
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-
-      //End code
-      });});}
-getData();
+const firebaseConfig = {
+      apiKey: "AIzaSyA28WZHfu7CgSytXbB-z3iVHepA5a3prW8",
+      authDomain: "kwitter-project-d9fe4.firebaseapp.com",
+      databaseURL: "https://kwitter-project-d9fe4-default-rtdb.firebaseio.com",
+      projectId: "kwitter-project-d9fe4",
+      storageBucket: "kwitter-project-d9fe4.appspot.com",
+      messagingSenderId: "690509519465",
+      appId: "1:690509519465:web:1bf5f7affb9b11ed9da0f8",
+      measurementId: "G-8WHVLNHLFB"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
